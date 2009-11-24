0a539dd1aa181a896072bc6f558de3c59222d3da
Vincent Siebert <vincent@siebert.im>
2009-11-24 17:20:50 +0000

    Added checks so that error notification emails dont get sent if we arent in the production environment

--- FeedsAggregator.php
+++ FeedsAggregator.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * It is the front controller for the whole aggregation 
+ * It is the front controller for the whole aggregation
  *
  * @package    lib.feedsaggregator
  */
@@ -13,7 +13,7 @@ class FeedsAggregator
 
   /**
    * The feeds to aggregate
-   * 
+   *
    * @access private
    * @var array of Feed Objects
    */
@@ -21,7 +21,7 @@ class FeedsAggregator
   /**
    * The name of the main class related to the objects created on the datastore from the feeds.
    * I.e.: GbJobs, SybDeal
-   * 
+   *
    * @access private
    * @var string
    */
@@ -29,7 +29,7 @@ class FeedsAggregator
   /**
    * A label for the environmnet the framework is running within (i.e.: 'dev', 'prod')
    * Useful for the error reporting.
-   * 
+   *
    * @access private
    * @var string
    */
@@ -37,9 +37,9 @@ class FeedsAggregator
 
   /**
    * Constructor
-   * 
+   *
    * @param array $feeds (see the class properties)
-   * @param string $mainClassName (see the class properties) 
+   * @param string $mainClassName (see the class properties)
    * @param $environment (see the class properties)
    */
   public function __construct($feeds, $mainClassName, $environment)
@@ -148,32 +148,35 @@ class FeedsAggregator
    * Sends logged errors via email.
    * It doesn't send an email for every error occured but it does use this class constant:
    * NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION
-   * 
+   *
    * @param string $msg the message to log
    * @param boolean $flush whether to flush the logs after appending the new message
    * @param string $environment  (see the class properties)
    */
   public static function reportError($msg = '', $flush = false, $environment = '')
   {
-    static $str;
-    static $counter = 0;
+    if (sfConfig::get('sf_environment') == 'prod')
+    {
+      static $str;
+      static $counter = 0;
 
-    if ($counter == self::NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION)
-    {
-      $flush = true;
-      $counter = 0;
-      $str = '';
-    }
+      if ($counter == self::NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION)
+      {
+        $flush = true;
+        $counter = 0;
+        $str = '';
+      }
 
-    if ($msg)
-    {
-      $str .= $msg . "\n\n\n||||||||||||||||||| END ERROR MESSAGE ||||||||||||||||||\n\n\n        ";
+      if ($msg)
+      {
+        $str .= $msg . "\n\n\n||||||||||||||||||| END ERROR MESSAGE ||||||||||||||||||\n\n\n        ";
+      }
+      if ($flush && $str)
+      {
+        mail('developers@studentbeans.com', "Feeds Aggregator error - {$_SERVER['HTTP_HOST']} - " . $environment, $str);
+        echo $str;
+      }
+      $counter++;
     }
-    if ($flush && $str)
-    {
-      mail('developers@studentbeans.com', "Feeds Aggregator error - {$_SERVER['HTTP_HOST']} - " . $environment, $str);
-      echo $str;
-    }
-    $counter++;
   }
 }
