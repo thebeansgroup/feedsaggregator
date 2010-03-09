dba6567fb4ac5414816819bad1eb2313c2ed3949
Daniele <daniele@thebeansgroup.com>
2010-03-09 17:56:18 +0000

    restructured some code for the feedsAggregation

--- FeedsAggregator.php
+++ FeedsAggregator.php
@@ -77,6 +77,64 @@ class FeedsAggregator
 
     foreach ($this->feeds as $feed)
     {
+        $this->aggregateFeed($feed);
+    }
+    FeedsAggregator::reportError('', true, $this->environment);
+    echo "\n\nAggregation completed.\n\n";
+    try
+    {
+      FeedsAggregatorEventManager::fire(FeedsAggregatorEventManager::END_AGGREGATION);
+    }
+    catch(Exception $e)
+    {
+      self::reportError("Error aggregating \n\n" . $e);
+    }
+    FeedsAggregatorEventManager::clearEvents();
+  }
+
+  /**
+   * Sends logged errors via email.
+   * It doesn't send an email for every error occured but it does use this class constant:
+   * NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION
+   *
+   * @param string $msg the message to log
+   * @param boolean $flush whether to flush the logs after appending the new message
+   * @param string $environment  (see the class properties)
+   */
+  public static function reportError($msg = '', $flush = false, $environment = '')
+  {
+    if (sfConfig::get('sf_environment') == 'prod')
+    {
+      static $str;
+      static $counter = 0;
+
+      if ($counter == self::NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION)
+      {
+        $flush = true;
+        $counter = 0;
+        $str = '';
+      }
+
+      if ($msg)
+      {
+        $str .= $msg . "\n\n\n||||||||||||||||||| END ERROR MESSAGE ||||||||||||||||||\n\n\n        ";
+      }
+      if ($flush && $str)
+      {
+        mail('developers@studentbeans.com', "Feeds Aggregator error - {$_SERVER['HTTP_HOST']} - " . $environment, $str);
+        echo $str;
+      }
+      $counter++;
+    }
+  }
+
+  /*
+   * Aggregates a single feed
+   *
+   * @param object $feed - the feed object to aggregate
+   */
+  private function aggregateFeed($feed)
+  {
       $processingStartTime = time();
       try
       {
@@ -131,53 +189,5 @@ class FeedsAggregator
         $feedHandler->deleteFeed();
       }
       $feed->recordProcessingTime(time() - $processingStartTime);
-    }
-    FeedsAggregator::reportError('', true, $this->environment);
-    echo "\n\nAggregation completed.\n\n";
-    try
-    {
-      FeedsAggregatorEventManager::fire(FeedsAggregatorEventManager::END_AGGREGATION);
-    }
-    catch(Exception $e)
-    {
-      self::reportError("Error aggregating \n\n" . $e);
-    }
-    FeedsAggregatorEventManager::clearEvents();
-  }
-
-  /**
-   * Sends logged errors via email.
-   * It doesn't send an email for every error occured but it does use this class constant:
-   * NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION
-   *
-   * @param string $msg the message to log
-   * @param boolean $flush whether to flush the logs after appending the new message
-   * @param string $environment  (see the class properties)
-   */
-  public static function reportError($msg = '', $flush = false, $environment = '')
-  {
-    if (sfConfig::get('sf_environment') == 'prod')
-    {
-      static $str;
-      static $counter = 0;
-
-      if ($counter == self::NUMBER_OF_ERROR_MESSAGES_BEFORE_SENDING_NOTIFICATION)
-      {
-        $flush = true;
-        $counter = 0;
-        $str = '';
-      }
-
-      if ($msg)
-      {
-        $str .= $msg . "\n\n\n||||||||||||||||||| END ERROR MESSAGE ||||||||||||||||||\n\n\n        ";
-      }
-      if ($flush && $str)
-      {
-        mail('developers@studentbeans.com', "Feeds Aggregator error - {$_SERVER['HTTP_HOST']} - " . $environment, $str);
-        echo $str;
-      }
-      $counter++;
-    }
   }
 }
